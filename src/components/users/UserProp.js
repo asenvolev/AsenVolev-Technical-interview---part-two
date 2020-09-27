1423db78c1b2d3b97cee5ae993c3cb1b2bfd0d54
asen.v <asen.v@sbtech.com>
2020-09-27 16:42:30 +0300

    disable inputs which are not editable

--- src/components/users/UserProp.js
+++ src/components/users/UserProp.js
@@ -1,40 +1,29 @@
 import React, { Fragment, useState } from 'react';
 
-const editableUserProps = ['name', 'username', 'email', 'street', 'city', 'phone', 'website'];
+const editableUserProps = ['name', 'username', 'email', 'address.street', 'address.suite', 'address.city', 'phone', 'website'];
 
 export const UserProp = ({objKey, objValue, onObjValueChanged}) => {
 
     if(typeof(objValue) == "object"){
         return Object.keys(objValue).map(prop => {
-            const propName = objKey + '.' + objValue;
+            const propName = objKey + '.' + prop;
             return <UserProp key={prop} objKey={propName} objValue={objValue[prop]} onObjValueChanged={onObjValueChanged}/>
         });
     }
 
-    let content;
-
-    if (editableUserProps.includes(objKey)) {
-        content = (<div>
-                    <label htmlFor={objKey}>{objKey}:</label>
-                    <input
-                        type="text"
-                        id={objKey}
-                        name={objKey}
-                        value={objValue}
-                        onChange={onObjValueChanged}
-                    />
-                    </div>)
-    }
-    else {
-        content = (<div>
-                    <label>{objKey}: {objValue.toString()}</label>
-                    </div>
-                    )
-    }
+   const isDisabled = !editableUserProps.includes(objKey);
 
     return( 
         <div>
-            {content}
+            <label htmlFor={objKey}>{objKey}:</label>
+            <input
+                type="text"
+                id={objKey}
+                name={objKey}
+                value={objValue}
+                onChange={onObjValueChanged}
+                disabled={isDisabled}
+            />
         </div>
     )
 }
